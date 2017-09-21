29aaccb488220542d19436ce71371cc07ddba0ea
Ishagoyal <iamishagoyal@gmail.com>
2017-09-21 17:14:49 +0530

    concept of this

--- components/Home.js
+++ components/Home.js
@@ -3,38 +3,29 @@ import { View, Text, FlatList, StyleSheet, Image } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import propertyListData from './data.json';
 
-
-
-function onPressPropertyName(item,e) {
-	e.preventDefault();
-	Actions.property({text: item.name});
-	console.log(item.name, 'was clicked');
-	
-}
-
 export default class Home extends Component{
 	render() {
 		return (
 			<View>
 				<FlatList
 					data={propertyListData}
-					renderItem={({item}) => renderPropertyListRow(item)}
-					//keyExtractor={(item, index) => index}
+					renderItem={(item) => this.renderPropertyListRow(item)}
 				/>
 			</View>
 		);
 	}
-}
 
-function renderPropertyListRow(item){
-	return(
-		<View >
-			<Text style={styles.item} key="a" onPress={onPressPropertyName.bind(this,item)}>{item.name} </Text>
-			<Text style={styles.item} key="b" > {item.price} </Text>
-			<Text style={styles.item} key="c" > {item.address} </Text>
-			<Image source={{uri:item.image}} key="d" style={{width: 32, height: 32}} />
-		</View>
-	);	 
+  onPressPropertyName(item){
+	Actions.property({text: item.name});
+	console.log(item.name, 'was clicked');	
+	}	
+
+
+  renderPropertyListRow(item){
+		return(
+			<Text style={styles.item} key="a" onPress={this.onPressPropertyName.bind(this,item)}>{item.name} </Text>
+		);	 
+	}
 }
 
 
