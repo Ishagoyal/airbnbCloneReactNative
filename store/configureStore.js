785480278076c8a50e7e274bd87f90c605d15224
Ishagoyal <iamishagoyal@gmail.com>
2018-04-28 18:11:08 +0530

    redux-persist setup in store again

--- store/configureStore.js
+++ store/configureStore.js
@@ -1,9 +1,16 @@
 import { createStore } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+
 import reducers from '../reducers';
 
-const store = createStore(reducers);
+const persistConfiq = {
+  key:  'root',
+  storage : storage,
+}
 
+const persistedReducer = persistReducer (persistConfiq, reducers);
 
-export default store;
+export const store = createStore(persistedReducer);
+export const persistor = persistStore(store);
+
