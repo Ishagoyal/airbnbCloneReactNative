61181b54e072ad86ec419126a7393b91fcc0da7b
Ishagoyal <iamishagoyal@gmail.com>
2018-05-02 15:43:11 +0530

    indentation corrected in index of actions

--- actions/index.js
+++ actions/index.js
@@ -3,34 +3,34 @@ import loginData from '../utils/userData.json';
 
 
 function login(username, password):Action{
-	if((loginData[0].username == username) && (loginData[0].password == password)){
-  	return {
-			type: actionType.LOGIN_SUCCESS,
-			username: username,
-  		password: password,
-		}
-	} 
-	else{
-		return{
-			type:actionType.LOGIN_FAILURE,
-			username: '',
-  		password: '',
-		}
-	}  
+  if((loginData[0].username == username) && (loginData[0].password == password)){
+    return {
+      type: actionType.LOGIN_SUCCESS,
+      username: username,
+      password: password,
+    }
+  } 
+  else{
+    return{
+      type:actionType.LOGIN_FAILURE,
+      username: '',
+      password: '',
+    }
+  }  
 }
 
 function logout():Action{
-	return{
-		type: actionType.LOGOUT,
-	}
-}	
+  return{
+    type: actionType.LOGOUT,
+  }
+} 
 
 function editProfile(firstName,lastName):Action{
-	return {
-		type:actionType.EDIT_PROFILE,
-		firstName: firstName,
-		lastName: lastName,
-	}
+  return {
+    type:actionType.EDIT_PROFILE,
+    firstName: firstName,
+    lastName: lastName,
+  }
 }
 
 
@@ -38,5 +38,5 @@ module.exports = {login,logout, editProfile};
 
 
 /*export const signup = (userName, password) => ({
-	type: actionType.SIGNUP,
+  type: actionType.SIGNUP,
 });*/
